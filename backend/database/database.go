4bd5199ad5e3eaff12fa6e25ab6d646d71d6f65c
Santiago <santi.rearte01@gmail.com>
2024-07-11 01:00:17 -0300

    Docker and docker files

--- backend/database/database.go
+++ backend/database/database.go
@@ -11,12 +11,12 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() {
-	// dsn_s := os.Getenv("DB_S")
+	dsn_s := os.Getenv("DB_S")
 	// dsn_i := os.Getenv("DB_I")
-	dsn_j := os.Getenv("DB_J")
-	// db, err := gorm.Open(mysql.Open(dsn_s), &gorm.Config{})
+	// dsn_j := os.Getenv("DB_J")
+	db, err := gorm.Open(mysql.Open(dsn_s), &gorm.Config{})
 	// db, err := gorm.Open(mysql.Open(dsn_i), &gorm.Config{})
-	db, err := gorm.Open(mysql.Open(dsn_j), &gorm.Config{})
+	// db, err := gorm.Open(mysql.Open(dsn_j), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to DB")
